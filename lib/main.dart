d98ee8f563b33617b6db9edf99c2eda90d6964f1
anay <njagidenzil@gmail.com>
2023-02-13 21:36:33 +0300

    home screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:ex_food_app/home/main_food_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -24,7 +25,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MainFoodPage(),
     );
   }
 }
